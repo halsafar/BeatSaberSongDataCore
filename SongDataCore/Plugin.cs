fa6a60ea53407e4e2ac34856887874d4be674699
Halsafar <421822+halsafar@users.noreply.github.com>
2019-12-17 16:24:43 -0600

    Version bump 1.2.0.

--- SongDataCore/Plugin.cs
+++ SongDataCore/Plugin.cs
@@ -10,7 +10,7 @@ namespace SongDataCore
 {
     public class Plugin : IBeatSaberPlugin
     {
-        public const string VERSION_NUMBER = "1.1.5";
+        public const string VERSION_NUMBER = "1.2.0";
         public static Plugin Instance;
         public static IPA.Logging.Logger Log;
 
