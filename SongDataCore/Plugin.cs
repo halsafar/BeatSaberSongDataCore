71a926f6a01a36243c02f9f8e9301659430eaf56
Halsafar <421822+halsafar@users.noreply.github.com>
2021-05-27 12:52:28 -0600

    Version bump for BeatSaber 1.60.0

--- SongDataCore/Plugin.cs
+++ SongDataCore/Plugin.cs
@@ -10,7 +10,7 @@ namespace SongDataCore
     [Plugin(RuntimeOptions.SingleStartInit)]
     public class Plugin
     {
-        public const string VERSION_NUMBER = "1.3.7";
+        public const string VERSION_NUMBER = "1.3.8";
         public static Plugin Instance;
         public static IPA.Logging.Logger Log;
 
